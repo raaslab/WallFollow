8f3d86119dff4fadd4c16dd437fbb5bcfa11d311
Kevin <klyu@vt.edu>
2019-01-08 16:50:59 -0500

    added new script for switching between functions

---
+++ scripts/bridgeFlight.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy
+import mavros
+import sensor_msgs
+import yaml
+from mavros_msgs.msg import *
+from mavros_msgs.srv import *
+from std_msgs.msg import String
+from std_msgs.msg import Int64
+from sensor_msgs.msg import NavSatFix
+
+
+
+
+
+
+
+
+def main():
+	rospy.init_node('bridgeFlight')
+
+
+
+if __name__ == '__main__':
+	main()
+
