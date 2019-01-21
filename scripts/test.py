467acc3fabe04121e92416d4aa70f568307a2de2
Kevin <klyu@vt.edu>
2019-01-21 13:25:26 -0500

    working on keypress/working on multi-threading

--- scripts/test.py
+++ scripts/test.py
@@ -1,31 +1,58 @@
 #!/usr/bin/env python
 
 import rospy
+from pynput import keyboard
+import threading
+
+
+
+
+
+# The key combination to check
+COMBINATIONS = [
+	{keyboard.KeyCode(char='1')}
+]
+
+# The currently active modifiers
+current = set()
+
+def execute():
+	print ("Do Something")
+
+def on_press(key):
+	if any([key in COMBO for COMBO in COMBINATIONS]):
+		current.add(key)
+		if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+			execute()
+	pass
+
+def on_release(key):
+	if any([key in COMBO for COMBO in COMBINATIONS]):
+		current.remove(key)
+	pass
 
 
 def main():
-	okay = 1
-	while okay == 1:
-		try:
-			while True:
-				print("1")
-				rospy.sleep(0.1)
-		except KeyboardInterrupt:
-			print("2")
+	try:
+		while True:
+			with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+				listener.join()
+
+			timerThread = threading.Thread(target= <your-function> )
+			timerThread.daemon = False # Is this a background process?
+			timerThread.start()
 
 
-	
-
-
-
-
-
-
-	rospy.sleep(1)
+	except KeyboardInterrupt:
+		print('interrupted!')
+		
 
 
 
 
 if __name__ == '__main__':
-	main()
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		pass
 	
